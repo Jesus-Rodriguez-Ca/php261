f405126fb501ff43bc8ce423e98c8c1fd0dd8d1e
Jesus Rodriguez <53617197+Jesus-Rodriguez-Ca@users.noreply.github.com>
2020-11-20 21:46:55 -0800

    Add files via upload

---
+++ website/contact.php
@@ -0,0 +1,23 @@
+<?php
+include('config.php'); 
+include('includes/header.php');
+?>
+
+
+    <div id="wrapper">
+        <main>
+        
+        <h1><?php echo $mainHeadline; ?></h1>
+    
+
+       <?php include('includes/form.php'); ?> 
+    </main>
+        <aside>
+        <h3>Link to GitHub</h3>
+            <a href="https://github.com/Jesus-Rodriguez-Ca/php261" class="fa fa-github" style="font-size: 36px "></a>
+        </aside>
+        
+<?php
+    include ('includes/footer.php');
+?>
+s
